68c0ed670baef93f9d1541456fb0a362c232965c
rajutalukdar1 <lalutaluker@gmail.com>
2023-01-18 23:48:20 +0600

    dashbord

---
+++ src/Page/Drawer/Drawer.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import { GoFileMedia } from "react-icons/go";
+// import { AiOutlineMessage, AiOutlineUser } from 'react-icons/ai';
+// import { Link } from 'react-router-dom';
+
+// const Drawer = () => {
+//     return (
+//         <div className='header'>
+//             <div>
+//                 <div className="drawer drawer-mobile">
+//                     <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
+
+//                     <div className="drawer-content mx-auto scrollBar">
+//                     </div>
+//                     <div className="drawer-side lg:block">
+//                         <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
+//                         <ul className="menu p-4 bg-base-100 text-base-content font-normal">
+
+//                             {/* <li className='rounded-md mb-4'><Link className='pl-0 text-2xl' to="/"><AiOutlineHome /> Home</Link></li> */}
+//                             <li className='rounded-md mb-4'><Link className='pl-0 text-2xl' to=""><GoFileMedia /> Media</Link></li>
+//                             <li className='rounded-md mb-4'><Link className='pl-0 text-2xl' to=""><AiOutlineMessage />Message</Link></li>
+//                             <li className='rounded-md mb-4'><Link className='pl-0 text-2xl' to=""><AiOutlineUser />About</Link></li>
+//                         </ul>
+
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default Drawer;
