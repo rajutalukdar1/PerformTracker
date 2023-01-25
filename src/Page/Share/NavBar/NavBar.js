cc5940ee0fd1cb9ed4606d1050b8b0076b96a1ea
Sumon Kaysar <msk.sumon.kaysar@gmail.com>
2023-01-25 23:54:03 +0600

    added blog page

--- src/Page/Share/NavBar/NavBar.js
+++ src/Page/Share/NavBar/NavBar.js
@@ -42,17 +42,9 @@ const NavBar = () => {
     </>
   );
   return (
-    <div className="stop">
-      <div className="navbar bg-base-100 flex justify-between">
+    <div className="stop bg-base-100 border-b-2">
+      <div className="navbar w-[95%] max-w-[1440px] mx-auto flex justify-between">
         <div className="navbar-start">
-          <div className="dropdown">
-            <label tabIndex={0} className="btn btn-ghost lg:hidden">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-            </label>
-            <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 " >
-              {menuItems}
-            </ul>
-          </div>
           <div className="navbar-start w-80">
             <Link className="flex items-center gap-3" to="/">
               <img className="w-8 sm:w-12 md:w-16" src={logo} alt="" />
@@ -67,9 +59,14 @@ const NavBar = () => {
             {menuItems}
           </ul>
         </div>
-        <label htmlFor="dashboard-drawer" tabIndex={2} className="btn btn-ghost lg:hidden">
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-        </label>
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </label>
+          <ul tabIndex={1} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-slate-100 rounded-box w-52 right-0" >
+            {menuItems}
+          </ul>
+        </div>
       </div>
     </div>
   );
--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,17 +1,16 @@
 import React, { createContext, useEffect, useState } from 'react';
-import {createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile} from 'firebase/auth'
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from 'firebase/auth'
 import app from '../firebase.config';
 
 
-
 export const AuthContext = createContext();
-    const auth = getAuth(app)
+const auth = getAuth(app)
 
-    const AuthProvider = ({children}) => {
+const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true)
 
-    const createUser = (email, password) =>{
+    const createUser = (email, password) => {
         setLoading(true)
         return createUserWithEmailAndPassword(auth, email, password);
     }
