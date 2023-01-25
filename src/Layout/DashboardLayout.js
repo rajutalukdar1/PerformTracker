fe1ea9b0db6f46cc64f149d09831260579f124b8
Ishtiaq <robin.pass36@gmail.com>
2023-01-26 00:43:09 +0600

    report section updated

--- src/Layout/DashboardLayout.js
+++ src/Layout/DashboardLayout.js
@@ -8,7 +8,7 @@ import DashboardNav from '../Page/Share/DashboardNav/DashboardNav';
 
 const DashboardLayout = () => {
     return (
-        <div>
+        <div className=''>
             <DashboardNav />
             <div className="drawer drawer-mobile pt-20">
                 <input id="dashboard-drawer" type="checkbox" className="drawer-toggle" />
@@ -17,24 +17,6 @@ const DashboardLayout = () => {
                 </div>
                 <div className="drawer-side fixed h-full w-full sm:w-[220px] overflow-y-auto">
                     <label htmlFor="dashboard-drawer" className="drawer-overlay"></label>
-<<<<<<< HEAD
-                    <ul className="menu p-4 bg-base-100 text-base-content font-normal">
-                        <div className="dropdown">
-                            <label tabIndex={0} className="btn m-1">Click</label>
-                            <div tabIndex={0} className="dropdown-content card card-compact w-64 p-2 
-                            shadow bg-primary text-primary-content">
-                                <ul tabIndex={0} className="dropdown-content menu p-2 shadow
-                                 bg-base-100 rounded-box w-52">
-                                    <li><a>Item 1</a></li>
-                                    <li><a>Item 2</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        {/* <li className='rounded-md mb-4'><Link className='pl-0 text-2xl' to="/"><AiOutlineHome /> Home</Link></li> */}
-                        <li className='rounded-md mb-4'><Link className='pl-0 text-2xl' to=""><AiOutlineUser />Employee</Link></li>
-                        <li className='rounded-md mb-4'><Link className='pl-0 text-2xl' to=""><HiOutlineUserGroup />Clients</Link></li>
-                        <li className='rounded-md mb-4'><Link className='pl-0 text-2xl' to="/dashboard/reports"><GoReport /> Reports</Link></li>
-=======
                     <ul className="menu bg-black text-base-content font-semibold py-5 w-full sm:w-[220px]">
                         <DashboardSideItems
                             icon={<AiOutlineUser className='text-2xl' />}
@@ -66,7 +48,6 @@ const DashboardLayout = () => {
                                 },
                             ]}
                         />
->>>>>>> 6d78451324e3bfce91601e65a8a7a9725b1225b0
                     </ul>
                 </div>
             </div>
