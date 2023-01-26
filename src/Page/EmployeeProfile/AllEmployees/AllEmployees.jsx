c0cfc15a2a465b1707ed13290b89472e831a9cc8
jabedit <sylhet.bd321@gmail.com>
2023-01-27 00:57:58 +0600

    post and update

--- src/Page/EmployeeProfile/AllEmployees/AllEmployees.jsx
+++ src/Page/EmployeeProfile/AllEmployees/AllEmployees.jsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect,  useState } from "react";
 import { Link } from "react-router-dom";
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { GrEdit } from 'react-icons/gr';
+import { RiDeleteBinLine } from 'react-icons/ri';
 import AddEmployeeFromModal from "./AddEmployeeFromModal";
+import EditEmployeeDetails from "./EditEmployeeDetails";
+import { useQuery } from "@tanstack/react-query";
 
 const AllEmployees = () => {
+  const [ employee , setEmployee] = useState(null)
   const [AllEmployees, setAllEmployees] = useState([]);
   console.log(AllEmployees);
 
@@ -11,6 +17,8 @@ const AllEmployees = () => {
       .then((res) => res.json())
       .then((data) => setAllEmployees(data));
   }, []);
+
+  
   return (
     <section className="md:px-20">
       <div className="flex  justify-between items-center mb-6">
@@ -21,19 +29,50 @@ const AllEmployees = () => {
         </div>
         <div>
           {/* The button to open modal */}
-          <label htmlFor="my-modal" className="px-3 py-2 bg-orange-600 rounded-tl-3xl text-white font-semibold text-xl">
-          Add Employee
+          <label
+            htmlFor="my-modal"
+            className="px-3 py-2 bg-orange-600 rounded-tl-3xl text-white font-semibold text-xl"
+          >
+            Add Employee
           </label>
           <AddEmployeeFromModal />
         </div>
       </div>
+      {
+        employee && <EditEmployeeDetails  employee={employee}/>
+      }
       <div
         style={{ backgroundColor: "#F7F7F7" }}
         className="grid  grid-cols-4 gap-6"
       >
         {AllEmployees.map((singleEmployee) => (
-          <Link to={`/dashboard/employee/${singleEmployee._id}`}>
-            <div className=" shadow-md  rounded-md   bg-white py-8">
+         
+            <div className=" shadow-md  rounded-sm   bg-white pb-8">
+              <div className="dropdown dropdown-bottom dropdown-right">
+             
+                {/* <label  className="btn bg-white border-0 ">
+                <span className="text-center"></span>
+                </label> */}
+                <div  tabIndex={0}>
+                  <h2  className="text-center mt-3 ml-4 font-semibold text-gray-500 text-xl"><BsThreeDotsVertical /> </h2>
+                </div>
+                
+                <ul
+                  tabIndex={0}
+                  className="dropdown-content menu p-2 border-2 bg-white rounded-box w-40"
+                >
+                  <li>
+                    
+                     <label onClick={()=> setEmployee(singleEmployee._id)} htmlFor="my-modal-2"><a> <GrEdit />edit</a> </label>
+                    
+                  </li>
+                  <li>
+                    <a> <RiDeleteBinLine />delete</a>
+                  </li>
+                </ul>
+                
+              </div>
+              <Link to={`/dashboard/employee/${singleEmployee._id}`}>
               <div className="flex  justify-center ">
                 <img
                   className="w-20 h-20 rounded-full"
@@ -49,8 +88,9 @@ const AllEmployees = () => {
                   {singleEmployee.designation}
                 </p>
               </div>
+              </Link>
             </div>
-          </Link>
+            
         ))}
       </div>
     </section>
---
+++ src/Page/EmployeeProfile/AllEmployees/EditEmployeeDetails.jsx
@@ -0,0 +1,222 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+import { toast } from 'react-hot-toast';
+
+const EditEmployeeDetails = ({employee}) => {
+
+
+    const { data: {name, address, designation, email, gender, img, maritalStatus, nationality, phone, salary}={}, refetch } = useQuery({
+        queryKey: ['employee', employee],
+        queryFn: () =>
+          fetch(`https://perform-tracker-server.vercel.app/employees/${employee}`).then(res => res.json()),
+      });
+      console.log(name)
+    const handleEditEmployee = (e) =>{
+        e.preventDefault()
+        const name = e.target.name.value
+        const email = e.target.email.value
+        const designation = e.target.designation.value
+        const address = e.target.address.value
+        const phone = e.target.phone.value
+        const salary = e.target.salary.value
+        const nationality = e.target.nationality.value
+        const gender = e.target.gender.value
+        const maritalStatus = e.target.maritalStatus.value
+        console.log(name,email,designation, address, phone, salary, nationality ,gender, maritalStatus , "data")
+    
+        const image =  e.target.employeeImg.files[0]
+        const formData = new FormData()
+        formData.append('image',image )
+       
+        const url = `https://api.imgbb.com/1/upload?key=dac9951f9d4ccd78dfaec90fb6367a69`
+        fetch(url, {
+            method: 'POST',
+            body: formData
+        })
+        .then(res => res.json())
+        .then(imgData => {
+           
+            if(imgData.success){
+                const post = {
+                    name: name,
+                    email: email,
+                    designation: designation,
+                    address: address,
+                    phone: phone,
+                    salary: salary,
+                    nationality: nationality,
+                    gender: gender,
+                    maritalStatus: maritalStatus,
+                    img: imgData.data.url,
+                    postTime: new Date().toLocaleString()
+                    
+                }
+                fetch(`https://perform-tracker-server.vercel.app/employees`, {
+                    method:'POST',
+                    headers: {
+                        'content-type' : 'application/json'
+    
+                    },
+                    body: JSON.stringify(post)
+                    
+                })
+                .then(res => res.json())
+                .then(result =>{
+                    console.log(result);
+                    toast.success('Employee is successfully added!')
+                    
+                   
+                })
+            }
+            
+        })
+        e.target.reset()
+    }
+      return (
+        <div>
+          
+    
+          <input type="checkbox" id="my-modal-2" className="modal-toggle" />
+         
+          <div className="modal">
+           
+            <div className="modal-box">
+                <h2>Update modal</h2>
+              <form onSubmit={handleEditEmployee} action="" class="space-y-4">
+              <label htmlFor="my-modal-2" className="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+              
+                <div className="mt-8">
+                  <label class="" for="name">
+                    Name
+                  </label>
+                  <input
+                    name="name"
+                    defaultValue={name}
+                    class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                    placeholder="Name"
+                    type="text"
+                    id="name"
+                  />
+                </div>
+                <div>
+                  <label class="sr-only" for="name">
+                    designation
+                  </label>
+                  <input
+                    name="designation"
+                    
+                    class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                    placeholder="designation"
+                    type="text"
+                    
+                  />
+                </div>
+                <div>
+                  <label class="sr-only" for="name">
+                    Address
+                  </label>
+                  <input
+                    name="address"
+                    class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                    placeholder="Address"
+                    type="text"
+                    
+                  />
+                </div>
+    
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                  <div>
+                    <label class="sr-only" for="email">
+                      Salary
+                    </label>
+                    <input
+                      name="salary"
+                      class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                      placeholder="Salary"
+                      type="text"
+                   
+                    />
+                  </div>
+    
+                  <div>
+                    <label class="sr-only">
+                      Nationality
+                    </label>
+                    <input
+                
+                      name="nationality"
+                      class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                      placeholder="nationality"
+                      type="text"
+                      
+                    />
+                  </div>
+                </div>
+    
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                  <div>
+                    <label class="sr-only" for="email">
+                      Email
+                    </label>
+                    <input
+                      name="email"
+                      class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                      placeholder="Email address"
+                      type="email"
+                      id="email"
+                    />
+                  </div>
+    
+                  <div>
+                    <label class="sr-only" for="phone">
+                      Phone
+                    </label>
+                    <input
+                      name="phone"
+                      class="w-full rounded-lg border-gray-200 p-3 text-sm"
+                      placeholder="Phone Number"
+                      type="tel"
+                      id="phone"
+                    />
+                  </div>
+                </div>
+                <div class="grid grid-cols-1 gap-4 sm:grid-cols-2">
+                  <div>
+                    <select name="gender"  className="select  rounded-lg border-gray-200 p-3 w-full max-w-xs">
+                      <option disabled selected>
+                        Select Gender
+                      </option>
+                      <option>Male</option>
+                      <option>Female</option>
+                      <option>Others</option>
+                     
+                    </select>
+                  </div>
+    
+                  <div>
+                    <select name="maritalStatus" className="select  rounded-lg border-gray-200 p-3 w-full max-w-xs">
+                      <option  disabled selected>
+                        Select marital Status
+                      </option>
+                      <option>unmarried</option>
+                      <option>married</option>
+                      
+                     
+                    </select>
+                  </div>
+                </div>
+                <div>
+                        <input name="employeeImg" type="file" placeholder="select your photo"  className="file-input file-input-bordered  w-full" />
+                        </div>
+                <div className="modal-action">
+                    <button className="px-3 py-2 rounded-lg bg-orange-600  text-white font-semibold text-xl" type="submit">Submit</button>
+                 
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      );
+    };
+
+export default EditEmployeeDetails;
