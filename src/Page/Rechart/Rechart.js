dfcdbb59fa36fcc13852a93c18769bfd05a17a9b
rajutalukdar1 <lalutaluker@gmail.com>
2023-01-31 22:24:25 +0600

    dashboard

---
+++ src/Page/Rechart/Rechart.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { Bar, BarChart, CartesianGrid, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+const Rechart = () => {
+    const data = [
+        {
+            "id": 1,
+            "name": "React",
+            "logo": "https://live.staticflickr.com/65535/52413593240_e00326e727_o.png",
+            "total": 8
+        },
+        {
+            "id": 2,
+            "name": "JavaScript",
+            "logo": "https://live.staticflickr.com/65535/52412638962_12e932256a_o.png",
+            "total": 9
+        },
+        {
+            "id": 4,
+            "name": "CSS",
+            "logo": "https://live.staticflickr.com/65535/52413665713_5977a693cb_o.png",
+            "total": 8
+        },
+        {
+            "id": 5,
+            "name": "Git",
+            "logo": "https://live.staticflickr.com/65535/52412639027_5692c15b3f_o.png",
+            "total": 11
+        }
+    ]
+    return (
+        <div>
+            <h2 className='font-bold text-4xl mt-10'>Employees Details</h2>
+            <div className='lg:flex mt-10'>
+                <BarChart width={400} height={400} data={data}>
+                    <Bar dataKey="total" fill="#8884d8" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                </BarChart>
+
+                <PieChart className='place-self-center' width={400} height={250} data={data}>
+                    <Pie data={data} dataKey="total" nameKey="name" cx="50%" cy="50%" outerRadius={50} fill="#8884d8" />
+                    <Pie data={data} dataKey="total" nameKey="name" cx="50%" cy="50%" innerRadius={60} outerRadius={80} fill="#82ca9d" label />
+                </PieChart>
+
+                <LineChart width={400} height={400} data={data}>
+                    <Line type="monotone" dataKey="total" stroke="#8884d8" />
+                    <CartesianGrid stroke="#ccc" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                </LineChart>
+            </div>
+        </div>
+    );
+};
+
+export default Rechart;
